1ee8968b5e66587f3c70ad2dff822e874db59f9d
erf <efremthx@gmail.com>
2026-03-13 09:38:37 +0100

    vez2

--- robo_vez.py
+++ robo_vez.py
@@ -13,19 +13,19 @@ x = pozice[0]
 y = pozice[1]
 z = pozice[2]
 r = pozice[3]
-vyska_kostka =-45
+vyska_kostka =-50
 posun = 60
 device.suck(False)
 for i in range(4):
-    pocet_kostek  = i * 30
+    pocet_kostek  = i * 25
     device.move_to(x,y+pocet_kostek,vyska_kostka,r,True)
     device.suck(True)
-    device.move_to(x,y,z,r,True)
-    device.move_to(x+posun,y,z,r,True)
+    device.move_to(x,y+pocet_kostek,z+60,r,True)
+    device.move_to(x+posun,y,z+60,r,True)
     device.move_to(x+posun,y,vyska_kostka+pocet_kostek,r,True)
     device.suck(False)
-    device.move_to(x+posun,y,z,r,True)
-    device.move_to(x,y,z,r,True)
+    device.move_to(x+posun,y,z+60,r,True)
+    device.move_to(x,y,z+60,r,True)
     print(i)
 
 device.close()
