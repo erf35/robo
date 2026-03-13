d4dac28cdba5e6256175c78e23076091297525e6
PolivkaZKari <kari23cz@gmail.com>
2026-03-13 09:05:27 +0100

    loop

---
+++ robo_vez_loop.py
@@ -0,0 +1,36 @@
+
+from serial.tools import list_ports
+
+import pydobot
+
+available_ports = list_ports.comports()
+print(f'available ports: {[x.device for x in available_ports]}')
+port = available_ports[1].device
+
+device = pydobot.Dobot(port=port, verbose=True)
+pozice = device.pose()
+x = pozice[0]
+y = pozice[1]
+z = pozice[2]
+r = pozice[3]
+vyska_kostka =-45
+posun = 60
+device.suck(False)
+i = 0
+while True:
+    if i < 4:
+        i+=1
+    else:
+        i = 0
+    pocet_kostek  = i * 30
+    device.move_to(x,y+pocet_kostek,vyska_kostka,r,True)
+    device.suck(True)
+    device.move_to(x,y,z,r,True)
+    device.move_to(x+posun,y,z,r,True)
+    device.move_to(x+posun,y,vyska_kostka+pocet_kostek,r,True)
+    device.suck(False)
+    device.move_to(x+posun,y,z,r,True)
+    device.move_to(x,y,z,r,True)
+    print(i)
+
+device.close()
