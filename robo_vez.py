d010432d9d607bc182f1da6332359cd6ec7de05d
PolivkaZKari <kari23cz@gmail.com>
2026-03-20 09:42:03 +0100

    loop15

--- robo_vez.py
+++ robo_vez.py
@@ -13,19 +13,27 @@ x = pozice[0]
 y = pozice[1]
 z = pozice[2]
 r = pozice[3]
-vyska_kostka =-50
+vyska_kostka =-47
 posun = 60
 device.suck(False)
-for i in range(4):
-    pocet_kostek  = i * 25
-    device.move_to(x,y+pocet_kostek,vyska_kostka,r,True)
-    device.suck(True)
-    device.move_to(x,y+pocet_kostek,z+60,r,True)
-    device.move_to(x+posun,y,z+60,r,True)
-    device.move_to(x+posun,y,vyska_kostka+pocet_kostek,r,True)
-    device.suck(False)
-    device.move_to(x+posun,y,z+60,r,True)
-    device.move_to(x,y,z+60,r,True)
-    print(i)
+i = 0
+flag = 0
+while True:
+    pocet_kostek  = i * 30
+    x_adjst = i*5
+    y_adjst = i*4
+    z_mov = 100
+    if flag == 0:
+        device.move_to(x-x_adjst,y+pocet_kostek-y_adjst,vyska_kostka,r,True)
+        device.suck(True)
+        device.move_to(x,y+pocet_kostek,z+z_mov,r,True)
+        device.move_to(x+posun,y,z+z_mov,r,True)
+        device.move_to(x+posun,y,vyska_kostka+pocet_kostek,r,True)
+        device.suck(False)
+        device.move_to(x+posun,y,z+z_mov,r,True)
+        device.move_to(x,y,z+z_mov,r,True)
+        i+=1
+        if i == 4:
+            i=0
 
 device.close()
