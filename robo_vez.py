cbd7ac0273d8b221ef653cab17e2cd774534c3da
PolivkaZKari <kari23cz@gmail.com>
2026-05-03 16:03:00 +0200

    vez okomentovane

--- robo_vez.py
+++ robo_vez.py
@@ -3,11 +3,14 @@ from serial.tools import list_ports
 
 import pydobot
 
-available_ports = list_ports.comports()
-print(f'available ports: {[x.device for x in available_ports]}')
-port = available_ports[0].device
 
+# inicializace
+available_ports = list_ports.comports()
+print(f'available ports: {[x.device for x in available_ports]}')   
+port = available_ports[0].device
 device = pydobot.Dobot(port=port, verbose=True)
+
+# urceni zakladnich pozic
 pozice = device.pose()
 x = pozice[0]
 y = pozice[1]
@@ -18,21 +21,31 @@ posun = 60
 device.suck(False)
 i = 0
 flag = 0
+
+# hlavni loop programu
 while True:
+    # posouvani pozic mezi jednotlivymi kostkami
     pocet_kostek  = i * 30
     x_adjst = i*5
     y_adjst = i*4
     z_mov = 100
     if flag == 0:
-        device.move_to(x-x_adjst,y+pocet_kostek-y_adjst,vyska_kostka,r,True)
-        device.suck(True)
-        device.move_to(x,y+pocet_kostek,z+z_mov,r,True)
+        device.move_to(x-x_adjst,y+pocet_kostek-y_adjst,vyska_kostka,r,True) # posun nad kostku ve skladu
+        device.suck(True) # spusteni prisavky
+
+        # posun kostky na vez
+        device.move_to(x,y+pocet_kostek,z+z_mov,r,True) 
         device.move_to(x+posun,y,z+z_mov,r,True)
         device.move_to(x+posun,y,vyska_kostka+pocet_kostek,r,True)
-        device.suck(False)
-        device.move_to(x+posun,y,z+z_mov,r,True)
+        device.suck(False) # vypnuti prisavky
+
+        # posun zpet nad sklad
+        device.move_to(x+posun,y,z+z_mov,r,True) 
         device.move_to(x,y,z+z_mov,r,True)
         i+=1
+
+        # kolikrat se ma loop opakovat
+        # for loop by byl lepsi, ale program byl vytvoren zkracenim ulohy 2B
         if i == 4:
             break
 
